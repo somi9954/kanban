15a61ae6a506deb24c8b73c05810a58e9b293195
somi9954 <jolh999@gmail.com>
2023-08-18 22:22:41 +0900

    의존성 추가

--- src/main/java/controllers/Controller.java
+++
@@ -1,9 +0,0 @@
-package controllers;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-public interface Controller {
-    void get(HttpServletRequest req, HttpServletResponse resp);
-    void post(HttpServletRequest req, HttpServletResponse resp);
-}
