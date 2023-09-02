50e764ee6c35d5072d6fc779fd89225e6c15806d
somi9954 <jolh999@gmail.com>
2023-09-02 23:44:51 +0900

    의존성 추가

--- src/main/java/filters/CommonRequestWrapper.java
+++ src/main/java/filters/CommonRequestWrapper.java
@@ -9,13 +9,14 @@ import models.member.Users;
 import java.io.IOException;
 
 public class CommonRequestWrapper extends HttpServletRequestWrapper {
+
     public CommonRequestWrapper(HttpServletRequest request, HttpServletResponse response) {
         super(request);
 
         // 요청 전 공통 처리 부분
         // 로그인 여부
         HttpSession session = request.getSession();
-        Users userInfo = (Users) session.getAttribute("users");
+        Users userInfo = (Users)session.getAttribute("users");
         boolean isLogin = userInfo != null;
         request.setAttribute("userInfo", userInfo);
         request.setAttribute("isLogin", isLogin);
@@ -24,12 +25,15 @@ public class CommonRequestWrapper extends HttpServletRequestWrapper {
         boolean isAccessible = true;
 
         String URI = request.getRequestURI();
-
-        // 회원 전용 URL - 미로그인 상태인 경우
+        String redirectURL = request.getContextPath();
+        String message = null;
+        // 회원전용 URL - 미로그인 상태인 경우
         if (!isLogin) {
             for (String url : getMemberOnlyUrls()) {
                 if (URI.indexOf(url) != -1) {
                     isAccessible = false;
+                    message = "회원전용 서비스 입니다.";
+                    redirectURL += "/member/login";
                 }
             }
         }
@@ -39,26 +43,33 @@ public class CommonRequestWrapper extends HttpServletRequestWrapper {
             for (String url : getGuestOnlyUrls()) {
                 if (URI.indexOf(url) != -1) {
                     isAccessible = false;
+                    message = "이미 로그인을 하셨습니다.";
+                    redirectURL += "/works";
                 }
             }
         }
-        if (!isAccessible) { //접근 권한 없음
+
+        if (!isAccessible) { // 접근 권한 없음
             try {
+                request.setAttribute("message", message);
+                request.setAttribute("redirectURL", redirectURL);
                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "접근 권한이 없습니다.");
             } catch (IOException e) {}
         }
     }
-    // 회원 전용 URL
+
+    // 회원전용 URL
     private String[] getMemberOnlyUrls() {
-        return  new String[] {
-          "/works"
+        return new String[] {
+                "/works"
         };
     }
+
     // 비회원 전용 URL
-    private String[] getGuestOnlyUrls () {
-      return  new String[] {
-        "member/login",
-        "/member/join"
-      };
+    private String[] getGuestOnlyUrls() {
+        return new String[] {
+                "/member/login",
+                "/member/join"
+        };
     }
 }
